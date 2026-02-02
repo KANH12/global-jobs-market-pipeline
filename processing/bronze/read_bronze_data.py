06e43665e7158b7c99e6fa72dde7234634a965f7
Bao Khang <baokhang1608@gmail.com>
2026-02-03 04:53:24 +0700

    Implement bronze ingestion pipeline for job data

---
+++ processing/bronze/read_bronze_data.py
@@ -0,0 +1,59 @@
+import logging
+from core.spark_session import create_spark_session
+from core.logger import get_job_logger
+# =========================
+# Logger setup
+# =========================
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+)
+
+logger = get_job_logger(
+    job_name="adzuna_bronze_reader",
+    component="bronze"
+    )
+
+
+def read_adzuna_bronze(spark, date_path: str):
+    path = f"s3a://data-lake/bronze/adzuna/{date_path}/*.json"
+
+    logger.info("📂 Reading bronze data")
+    logger.info(f"📅 date_path={date_path}")
+    logger.info(f"📁 source_path={path}")
+
+    df = (
+        spark.read
+        .option("multiLine", True)
+        .json(path)
+    )
+
+    return df
+
+
+if __name__ == "__main__":
+    logger.info("🚀 START adzuna bronze reader")
+
+    spark = create_spark_session()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    date_path = "2026/02/02"
+
+    try:
+        df = read_adzuna_bronze(spark, date_path)
+
+        record_count = df.count()
+        logger.info(f"📊 Record count={record_count}")
+
+        logger.info("📐 Schema:")
+        df.printSchema()
+
+        logger.info("✅ Bronze read completed successfully")
+
+    except Exception as e:
+        logger.error("❌ Failed to read bronze data", exc_info=True)
+        raise
+
+    finally:
+        spark.stop()
+        logger.info("🛑 Spark session stopped")
