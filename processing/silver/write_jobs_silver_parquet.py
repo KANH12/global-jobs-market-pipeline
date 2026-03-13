cff74e6e0c93f4da7742f0095f2d44e045f754d3
Bao Khang <baokhang1608@gmail.com>
2026-03-14 00:54:16 +0700

    change location silver file and fix write_jobs_silver_parquet.py

--- processing/silver/write_jobs_silver_parquet.py
+++ processing/silver/write_jobs_silver_parquet.py
@@ -1,30 +1,36 @@
 from pyspark.sql import functions as F
 from core.spark_session import create_spark_session
 from core.logger import get_job_logger
+from processing.bronze import read_bronze_data
 
 logger = get_job_logger(
     job_name="adzuna_jobs_silver_writer",
     component="silver"
 )
 
-def write_jobs_silver(df, date_path: str):
-    logger.info("🚀 START Silver jobs transform")
-
+def tranform_bronze_to_silver(df_bronze):
     # =========================
     # 0. Explode bronze records → job-level
     # =========================
     df = (
-        df
+        df_bronze
         .filter(F.col("records").isNotNull())
         .select(F.explode("records").alias("job"))
+        .select("job.*")
     )
+    return df
+
+def process_silver(df, date_path):
+
+    logger.info(f"🚀 START Silver pipeline | date={date_path}")
 
     # =========================
     # 1. Filter invalid jobs
     # =========================
+    df = df.cache()
     before_count = df.count()
 
-    df = df.filter(F.col("job.id").isNotNull())
+    df = df.filter(F.col("id").isNotNull())
 
     after_count = df.count()
     logger.info(f"🧹 Removed jobs with NULL id: {before_count - after_count}")
@@ -33,79 +39,86 @@ def write_jobs_silver(df, date_path: str):
     # 2. Select & transform columns
     # =========================
     jobs_df = df.select(
-        F.col("job.id").alias("job_id"),
-        F.col("job.title"),
+        F.col("id").alias("job_id"),
+        F.col("title"),
         
         F.when(
-            F.col("job.contract_time").isNull(),
+            F.col("contract_time").isNull(),
             F.lit("UNKNOWN")
         ).otherwise(
-            F.upper(F.trim(F.col("job.contract_time")))
+            F.upper(F.trim(F.col("contract_time")))
         ).alias("contract_time"),
         
         F.when(
-            F.col("job.contract_type").isNull(),
+            F.col("contract_type").isNull(),
             F.lit("UNKNOWN")
         ).otherwise(
-            F.upper(F.trim(F.col("job.contract_type")))
+            F.upper(F.trim(F.col("contract_type")))
         ).alias("contract_type"),
 
-        F.col("job.created").cast("timestamp"),
-        F.col("job.salary_min"),
-        F.col("job.salary_max"),
-        (F.col("job.salary_is_predicted") == "1").alias("salary_is_predicted"),
+        F.col("created").cast("timestamp"),
+        F.col("salary_min"),
+        F.col("salary_max"),
+        (F.col("salary_is_predicted") == "1").alias("salary_is_predicted"),
 
-        F.col("job.latitude"),
-        F.col("job.longitude"),
+        F.col("latitude"),
+        F.col("longitude"),
 
         # ---------- CATEGORY ----------
-        F.col("job.category.label").alias("category_label"),
-        F.col("job.category.tag").alias("category_tag"),
+        F.col("category.label").alias("category_label"),
+        F.col("category.tag").alias("category_tag"),
         F.sha2(
             F.concat_ws(
                 "||",
-                F.col("job.category.label"),
-                F.col("job.category.tag")
+                F.col("category.label"),
+                F.col("category.tag")
             ),
             256
         ).alias("category_id"),
 
         # ---------- COMPANY ----------
-        F.col("job.company.display_name").alias("company_name"),
+        F.col("company.display_name").alias("company_name"),
         F.sha2(
-            F.col("job.company.display_name"),
+            F.col("company.display_name"),
             256
         ).alias("company_id"),
 
         # ---------- LOCATION ----------
-        F.col("job.location.display_name").alias("location_name"),
+        F.col("location.display_name").alias("location_name"),
         F.sha2(
             F.concat_ws(
                 "||",
-                F.col("job.location.display_name"),
-                F.concat_ws(",", F.col("job.location.area"))
+                F.col("location.display_name"),
+                F.concat_ws(",", F.col("location.area"))
             ),
             256
         ).alias("location_id"),
+        F.lit(date_path).alias("ingestion_date")
     )
 
     logger.info("✅ Selected & transformed columns")
 
+    return jobs_df
+
     # =========================
     # 3. Write Parquet
     # =========================
+
+def write_jobs_silver(jobs_df, date_path: str):
     output_path = f"s3a://data-lake/silver/adzuna/jobs/dt={date_path}"
 
-    logger.info(f"📂 Writing jobs parquet to {output_path}")
+    #avoid small files problem
+    jobs_df = jobs_df.repartition(4)
 
+    logger.info(f"📂 Writing jobs parquet to {output_path}")
+    count = jobs_df.count()
     (
         jobs_df
         .write
         .mode("overwrite")
         .parquet(output_path)
     )
-
-    logger.info(f"📊 Output records: {jobs_df.count()}")
+    logger.info(f"📊 Output records: {count}")
     logger.info("🎉 Silver jobs parquet written successfully")
 
 
@@ -114,20 +127,26 @@ if __name__ == "__main__":
     spark.sparkContext.setLogLevel("ERROR")
 
     date_path = "2026/02/04"
-
+    
     try:
-        df = (
-            spark.read
-            .option("multiLine", True)
-            .json(f"s3a://data-lake/bronze/adzuna/{date_path}/*.json")
-        )
+        #1. read bronze layer
+        df_bronze = read_bronze_data(spark, date_path)
 
-        write_jobs_silver(df, date_path)
+        #2. transform bronze layer to silver layer
+        df = tranform_bronze_to_silver(df_bronze)
 
+        #3. process silver layer
+        jobs_df = process_silver(df, date_path)
+
+        #4. write silver parquet file
+        write_jobs_silver(jobs_df, date_path)
+        
     except Exception:
-        logger.error("❌ Silver jobs write failed", exc_info=True)
+        logger.error("❌ Silver jobs running failed", exc_info=True)
         raise
-
+        
     finally:
         spark.stop()
         logger.info("🛑 Spark stopped")
+
+    
