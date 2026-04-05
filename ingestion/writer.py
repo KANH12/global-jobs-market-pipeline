8fc788a135de5980c95b982b776875e08f1275ef
Bao Khang <baokhang1608@gmail.com>
2026-04-06 01:23:54 +0700

    Modified File and setup env

--- ingestion/writer.py
+++ ingestion/writer.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from io import BytesIO
 from minio import Minio
 from minio.error import S3Error
-
+import os
 
 # -------------------------
 # MinIO client
@@ -11,11 +11,10 @@ from minio.error import S3Error
 def get_minio_client():
     return Minio(
         endpoint="minio:9000",
-        access_key="minioadmin",
-        secret_key="minioadmin",
+        access_key=os.getenv("MINIO_ACCESS_KEY"),
+        secret_key=os.getenv("MINIO_SECRET_KEY"),
         secure=False
     )
-
 
 # -------------------------
 # Ensure bucket exists
